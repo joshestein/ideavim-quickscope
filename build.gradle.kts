3dd1e240870616f1ba0ed1e8f65f5c6379d113fb
Josh <josh.e.stein@gmail.com>
2022-06-20 21:26:22 +0200

    Initialize plugin

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.intellij") version "1.5.2"
+}
+
+group = "com.joshestein"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    version.set("2021.2")
+//    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("IdeaVIM:1.9.2"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("212")
+        untilBuild.set("222.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
---
+++ src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
@@ -0,0 +1,4 @@
+package com.joshestein.ideavimquickscope
+
+class IdeaVimQuickscopeExtension {
+}
