e46e4b2b683e51e071b99ffa483ddf222892c050
Josh <josh.e.stein@gmail.com>
2022-06-21 23:26:33 +0200

    Initialization

--- src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
@@ -1,4 +1,19 @@
 package com.joshestein.ideavimquickscope
 
 class IdeaVimQuickscopeExtension {
+class IdeaVimQuickscopeExtension : VimExtension {
+
+    override fun getName() = "quickscope";
+    override fun init() {
+        // TODO: NVO?
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-find"), owner, QuickscopeHandler("f"), false);
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-to"), owner, QuickscopeHandler("t"), false);
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-find"), owner, QuickscopeHandler("F"), false);
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-to"), owner, QuickscopeHandler("T"), false);
+
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("f"), owner, parseKeys("<Plug>quickscope-forward-find"), true);
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("t"), owner, parseKeys("<Plug>quickscope-forward-to"), true);
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("F"), owner, parseKeys("<Plug>quickscope-backward-find"), true);
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("T"), owner, parseKeys("<Plug>quickscope-backward-to"), true);
+    }
 }
