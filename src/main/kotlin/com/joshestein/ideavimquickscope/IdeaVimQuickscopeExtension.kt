b557df381075d96b537deb10854bbe1cb771458c
Josh <josh.e.stein@gmail.com>
2022-06-22 20:39:11 +0200

    Cleanup and formatting

--- src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
@@ -24,77 +24,77 @@ private val log = logger<IdeaVimQuickscopeExtension>()
 
 enum class Direction { FORWARD, BACKWARD }
 
-val ACCEPTED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+val ACCEPTED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray()
 
 class IdeaVimQuickscopeExtension : VimExtension {
 
-    override fun getName() = "quickscope";
+    override fun getName() = "quickscope"
     override fun init() {
         // TODO: HIGHLIGHT_ON_KEYS
         // @formatter:off
-        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-find"), owner, QuickscopeHandler('f'), false);
-        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-to"), owner, QuickscopeHandler('t'), false);
-        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-find"), owner, QuickscopeHandler('F'), false);
-        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-to"), owner, QuickscopeHandler('T'), false);
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-find"), owner, QuickscopeHandler('f'), false)
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-forward-to"), owner, QuickscopeHandler('t'), false)
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-find"), owner, QuickscopeHandler('F'), false)
+        putExtensionHandlerMapping(MappingMode.NXO, parseKeys("<Plug>quickscope-backward-to"), owner, QuickscopeHandler('T'), false)
 
-        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("f"), owner, parseKeys("<Plug>quickscope-forward-find"), true);
-        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("t"), owner, parseKeys("<Plug>quickscope-forward-to"), true);
-        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("F"), owner, parseKeys("<Plug>quickscope-backward-find"), true);
-        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("T"), owner, parseKeys("<Plug>quickscope-backward-to"), true);
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("f"), owner, parseKeys("<Plug>quickscope-forward-find"), true)
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("t"), owner, parseKeys("<Plug>quickscope-forward-to"), true)
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("F"), owner, parseKeys("<Plug>quickscope-backward-find"), true)
+        putKeyMappingIfMissing(MappingMode.NXO, parseKeys("T"), owner, parseKeys("<Plug>quickscope-backward-to"), true)
         // @formatter:on
     }
 
     private class QuickscopeHandler(private val char: Char) : VimExtensionHandler {
-        private val highlighters: MutableSet<RangeHighlighter> = mutableSetOf();
+        private val highlighters: MutableSet<RangeHighlighter> = mutableSetOf()
 
-        lateinit var editor: Editor;
+        lateinit var editor: Editor
 
         override fun execute(editor: Editor, context: DataContext) {
-            val direction = if (char == 'f' || char == 't') Direction.FORWARD else Direction.BACKWARD;
-            this.editor = editor;
+            val direction = if (char == 'f' || char == 't') Direction.FORWARD else Direction.BACKWARD
+            this.editor = editor
 
-            addHighlights(direction);
-            val to = getChar(editor);
+            addHighlights(direction)
+            val to = getChar()
             if (to == null) {
-                removeHighlights();
-                return;
+                removeHighlights()
+                return
             }
-            VimExtensionFacade.executeNormalWithoutMapping(parseKeys(char.toString()), editor);
-            VimExtensionFacade.executeNormalWithoutMapping(parseKeys(to.toString()), editor);
-            removeHighlights();
+            VimExtensionFacade.executeNormalWithoutMapping(parseKeys(char.toString()), editor)
+            VimExtensionFacade.executeNormalWithoutMapping(parseKeys(to.toString()), editor)
+            removeHighlights()
         }
 
-        private fun getChar(editor: Editor): Char? {
+        private fun getChar(): Char? {
             val key = VimExtensionFacade.inputKeyStroke(this.editor)
-            if (key.keyChar == KeyEvent.CHAR_UNDEFINED || key.keyCode == KeyEvent.VK_ESCAPE) return null;
+            if (key.keyChar == KeyEvent.CHAR_UNDEFINED || key.keyCode == KeyEvent.VK_ESCAPE) return null
             return key.keyChar
         }
 
         private fun addHighlights(direction: Direction) {
-            val occurences = mutableMapOf<Char, Int>();
-            var (highlight_primary, highlight_secondary) = Pair(0, 0);
+            val occurrences = mutableMapOf<Char, Int>()
+            var (highlight_primary, highlight_secondary) = Pair(0, 0)
 
-            val caret = this.editor.caretModel.primaryCaret;
+            val caret = this.editor.caretModel.primaryCaret
             val text = if (direction == Direction.FORWARD) {
-                this.editor.document.getText(TextRange(caret.offset, caret.visualLineEnd));
+                this.editor.document.getText(TextRange(caret.offset, caret.visualLineEnd))
             } else {
                 // TODO: backwards
-                this.editor.document.getText(TextRange(caret.visualLineStart, caret.offset));
+                this.editor.document.getText(TextRange(caret.visualLineStart, caret.offset))
             }
 
-            var newWord = true;
+            var newWord = true
             for ((i, c) in text.withIndex()) {
                 if (ACCEPTED_CHARS.contains(c)) {
-                    occurences[c] = occurences.getOrDefault(c, 0) + 1;
-                    val occurence = occurences[c];
+                    occurrences[c] = occurrences.getOrDefault(c, 0) + 1
+                    val occurrence = occurrences[c]
 
 //                    if (!newWord && (occurence == 1 || occurence == 2)) {
-                    if (!newWord && occurence == 1) {
-                        addHighlight(caret.offset + i, occurence == 1);
-                        newWord = true;
+                    if (!newWord && occurrence == 1) {
+                        addHighlight(caret.offset + i, occurrence == 1)
+                        newWord = true
                     }
                 } else {
-                    newWord = false;
+                    newWord = false
                 }
             }
         }
@@ -106,8 +106,8 @@ class IdeaVimQuickscopeExtension : VimExtension {
                 HighlighterLayer.SELECTION,
                 getHighlightTextAttributes(primary),
                 HighlighterTargetArea.EXACT_RANGE
-            );
-            highlighters.add(highlight);
+            )
+            highlighters.add(highlight)
         }
 
         private fun getHighlightTextAttributes(primary: Boolean) = TextAttributes(
@@ -121,9 +121,9 @@ class IdeaVimQuickscopeExtension : VimExtension {
 
         private fun removeHighlights() {
             highlighters.forEach { highlighter ->
-                this.editor.markupModel.removeHighlighter(highlighter);
+                this.editor.markupModel.removeHighlighter(highlighter)
             }
-            highlighters.clear();
+            highlighters.clear()
         }
     }
 }
