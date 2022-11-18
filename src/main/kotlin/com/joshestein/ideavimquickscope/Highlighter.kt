04775cbb23b1ddc3454511b77186ee0d6819113d
Josh <josh.e.stein@gmail.com>
2022-11-18 23:38:01 +0100

    Create `Highlighter` class
    Delegated responsibilities of tracking highlights.

---
+++ src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
@@ -0,0 +1,41 @@
+package com.joshestein.ideavimquickscope
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.markup.*
+import java.awt.Font
+
+data class Highlight(val position: Int, val primary: Boolean)
+
+class Highlighter(private val editor: Editor) {
+    private val highlighters: MutableSet<RangeHighlighter> = mutableSetOf()
+
+    fun addHighlights(highlights: List<Highlight>) {
+        highlights.forEach { highlight ->
+            highlighters.add(
+                this.editor.markupModel.addRangeHighlighter(
+                    highlight.position,
+                    highlight.position + 1,
+                    HighlighterLayer.SELECTION,
+                    getHighlightTextAttributes(highlight.primary),
+                    HighlighterTargetArea.EXACT_RANGE
+                )
+            )
+        }
+    }
+
+    private fun getHighlightTextAttributes(primary: Boolean) = TextAttributes(
+        null,
+        if (primary) EditorColors.TEXT_SEARCH_RESULT_ATTRIBUTES.defaultAttributes.backgroundColor else EditorColors.DELETED_TEXT_ATTRIBUTES.defaultAttributes.backgroundColor,
+        this.editor.colorsScheme.getColor(EditorColors.CARET_COLOR),
+        EffectType.LINE_UNDERSCORE,
+        Font.PLAIN
+    )
+
+    fun removeHighlights() {
+        highlighters.forEach { highlighter ->
+            this.editor.markupModel.removeHighlighter(highlighter)
+        }
+        highlighters.clear()
+    }
+}
