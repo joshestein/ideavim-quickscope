3b7284bf422cf5fdc40e5a2ab410397e3d6bfe89
Josh <josh.e.stein@gmail.com>
2022-11-22 21:22:44 +0100

    Allow customization of highlight colors

--- src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
@@ -3,11 +3,30 @@ package com.joshestein.ideavimquickscope
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.EditorColors
 import com.intellij.openapi.editor.markup.*
+import com.maddyhome.idea.vim.VimPlugin
+import java.awt.Color
 import java.awt.Font
+
+private const val PRIMARY_COLOR_VARIABLE = "qs_primary_color"
+private const val SECONDARY_COLOR_VARIABLE = "qs_secondary_color"
+
+private val DEFAULT_PRIMARY_COLOR = EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor
+private val DEFAULT_SECONDARY_COLOR =
+    EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor.brighter()
 
 data class Highlight(val position: Int, val primary: Boolean)
 
 class Highlighter(var editor: Editor) {
+    private var primaryColor: Color? = try {
+        Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(PRIMARY_COLOR_VARIABLE).toString())
+    } catch (e: Exception) {
+        DEFAULT_PRIMARY_COLOR
+    }
+    private var secondaryColor: Color? = try {
+        Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(SECONDARY_COLOR_VARIABLE).toString())
+    } catch (e: Exception) {
+        DEFAULT_SECONDARY_COLOR
+    }
     private val highlighters: MutableSet<RangeHighlighter> = mutableSetOf()
 
     fun updateEditor(editor: Editor) {
