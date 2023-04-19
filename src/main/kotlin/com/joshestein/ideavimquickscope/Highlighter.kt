44cf04c88119ab6c6ced3273a9197f8f7da58cd1
Matt Ellis <m.t.ellis@gmail.com>
2023-04-19 15:19:28 +0100

    Use theme colours and handle theme changes

--- src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
@@ -10,21 +10,18 @@ import java.awt.Font
 private const val PRIMARY_COLOR_VARIABLE = "qs_primary_color"
 private const val SECONDARY_COLOR_VARIABLE = "qs_secondary_color"
 
-private val DEFAULT_PRIMARY_COLOR = EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor
-private val DEFAULT_SECONDARY_COLOR = DEFAULT_PRIMARY_COLOR.brighter()
-
 data class Highlight(val position: Int, val primary: Boolean)
 
 class Highlighter(var editor: Editor) {
     private var primaryColor: Color? = try {
         Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(PRIMARY_COLOR_VARIABLE).toString())
     } catch (e: Exception) {
-        DEFAULT_PRIMARY_COLOR
+        null
     }
     private var secondaryColor: Color? = try {
         Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(SECONDARY_COLOR_VARIABLE).toString())
     } catch (e: Exception) {
-        DEFAULT_SECONDARY_COLOR
+        null
     }
     private val highlighters: MutableSet<RangeHighlighter> = mutableSetOf()
 
@@ -34,26 +31,39 @@ class Highlighter(var editor: Editor) {
     }
 
     fun addHighlights(highlights: List<Highlight>) {
+        val primary = getPrimaryHighlightTextAttributes()
+        val secondary = getSecondaryHighlightTextAttributes()
+
         highlights.forEach { highlight ->
             highlighters.add(
                 this.editor.markupModel.addRangeHighlighter(
                     highlight.position,
                     highlight.position + 1,
                     HighlighterLayer.SELECTION,
-                    getHighlightTextAttributes(highlight.primary),
+                    if (highlight.primary) primary else secondary,
                     HighlighterTargetArea.EXACT_RANGE
                 )
             )
         }
     }
 
-    private fun getHighlightTextAttributes(primary: Boolean) = TextAttributes(
-        if (primary) primaryColor else secondaryColor,
-        null,
-        if (primary) primaryColor else secondaryColor,
-        EffectType.LINE_UNDERSCORE,
-        Font.PLAIN
-    )
+    private fun getPrimaryHighlightTextAttributes(): TextAttributes {
+        // Get the fallback color each time, so we cope with theme changes. We can't do anything for user configured
+        // colors without some autocmd mechanism
+        val color = primaryColor ?: run {
+            editor.colorsScheme.getAttributes(EditorColors.REFERENCE_HYPERLINK_COLOR)?.foregroundColor
+                ?: EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor
+        }
+        return TextAttributes(color, null, color, EffectType.LINE_UNDERSCORE, Font.PLAIN)
+    }
+
+    private fun getSecondaryHighlightTextAttributes(): TextAttributes {
+        val color = secondaryColor ?: run {
+            (editor.colorsScheme.getAttributes(EditorColors.REFERENCE_HYPERLINK_COLOR)?.foregroundColor
+            ?: EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor).brighter()
+        }
+        return TextAttributes(color, null, color, EffectType.LINE_UNDERSCORE, Font.PLAIN)
+    }
 
     fun removeHighlights() {
         highlighters.forEach { highlighter ->
