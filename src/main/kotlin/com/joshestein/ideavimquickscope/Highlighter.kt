04674609dc04465b931410d3309c01b8829036f7
Josh <josh.e.stein@gmail.com>
2023-04-21 19:57:46 +0200

    Prefer colors as `val` instead of `var`

--- src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/Highlighter.kt
@@ -13,13 +13,13 @@ private const val SECONDARY_COLOR_VARIABLE = "qs_secondary_color"
 data class Highlight(val position: Int, val primary: Boolean)
 
 class Highlighter(var editor: Editor) {
-    private var primaryColor: Color = try {
+    private val primaryColor: Color = try {
         Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(PRIMARY_COLOR_VARIABLE).toString())
     } catch (e: Exception) {
         editor.colorsScheme.getAttributes(EditorColors.REFERENCE_HYPERLINK_COLOR)?.foregroundColor
             ?: EditorColors.REFERENCE_HYPERLINK_COLOR.defaultAttributes.foregroundColor
     }
-    private var secondaryColor: Color = try {
+    private val secondaryColor: Color = try {
         Color.decode(VimPlugin.getVariableService().getGlobalVariableValue(SECONDARY_COLOR_VARIABLE).toString())
     } catch (e: Exception) {
         (editor.colorsScheme.getAttributes(EditorColors.REFERENCE_HYPERLINK_COLOR)?.foregroundColor
