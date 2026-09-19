c6a2d9947f7f07b11381d31c5482162790eb2be8
Josh Stein <josh.e.stein@gmail.com>
2026-09-19 15:06:47 +0200

    Blocker for `highlightKeys` being VimList

--- src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
+++ src/main/kotlin/com/joshestein/ideavimquickscope/IdeaVimQuickscopeExtension.kt
@@ -112,6 +112,8 @@ class IdeaVimQuickscopeExtension : VimExtension {
         val parent = Disposer.newDisposable("IdeaVim-Quickscope")
         disposable = parent
         val multicaster = EditorFactory.getInstance().eventMulticaster
+
+        if (highlightKeys is VimList) {
             // Only add highlights after pressing one of the variable keys (e.g. "f", "t", "F", "T")
             for (value in highlightKeys.values) {
                 // TODO: When using a newer version of IdeaVim, we can use value.toVimString().value
